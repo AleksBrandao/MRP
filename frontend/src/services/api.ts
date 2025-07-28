00871e39f5da46b90ffe885e9318c5cb30fb102f
Aleksander Brandão <58121150+AleksBrandao@users.noreply.github.com>
2025-07-28 22:08:56 +0000

    Implantação no Codespace

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // ajuste se usar ngrok ou IP da rede local
+  baseURL: import.meta.env.VITE_API_URL, // ajuste se usar ngrok ou IP da rede local
 });
 
 export default api;
